176e669a660ba77970461ef002c11fc0cb495097
Rodrigo Cid <rodrigocdi95@gmail.com>
2025-10-25 19:49:49 -0600

    Add port of env

--- server/main.go
+++ server/main.go
@@ -8,6 +8,7 @@ import (
 	"embed"
 	"encoding/gob"
 	"net/http"
+	"os"
 	"path/filepath"
 
 	"github.com/gorilla/sessions"
@@ -21,6 +22,7 @@ import (
 var assets embed.FS
 
 var mainDirs []string = []string{
+	filepath.Join(".", ".data"),
 	filepath.Join(".", ".data", "server"),
 	filepath.Join(".", ".data", "server", "certs"),
 	filepath.Join(".", ".data", "server", "sessions"),
@@ -33,47 +35,60 @@ func main() {
 		}
 	}
 
-	key := utils.ReadFile(certificate.KeyPath)
-	cookieStore := sessions.NewFilesystemStore(mainDirs[2], key)
-
 	gob.Register(models.User{})
 
-	httpServer := echo.New()
-	httpServer.Pre(redirectToHTTPS())
-	httpServer.Use(middleware.Logger())
-	httpServer.Use(middleware.Recover())
-	httpServer.Logger.SetLevel(log.INFO)
+	port := os.Getenv("PORT")
 
-	httpsServer := echo.New()
-	httpsServer.Use(session.Middleware(cookieStore))
-	httpsServer.Use(middleware.Logger())
-	httpsServer.Use(middleware.Recover())
-	httpsServer.Logger.SetLevel(log.INFO)
+	if port == "" {
+		httpServer := echo.New()
+		httpServer.Pre(redirectToHTTPS())
+		httpServer.Use(middleware.Logger())
+		httpServer.Use(middleware.Recover())
+		httpServer.Logger.SetLevel(log.INFO)
+
+		httpsServer := echo.New()
+		setRoutes(httpsServer)
+
+		go func() {
+			httpServer.Logger.Fatal(httpServer.Start(":80"))
+		}()
+
+		if !utils.FileExists(certificate.CertPath) || !utils.FileExists(certificate.KeyPath) {
+			certificate.Generate()
+		}
+
+		httpsServer.Logger.Fatal(httpsServer.StartTLS(":443", certificate.CertPath, certificate.KeyPath))
+	} else {
+		httpServer := echo.New()
+		setRoutes(httpServer)
+
+		httpServer.Logger.Fatal(httpServer.Start(":" + port))
+	}
+}
+
+func setRoutes(e *echo.Echo) {
+	key := utils.ReadFile(certificate.KeyPath)
+	cookieStore := sessions.NewFilesystemStore(mainDirs[2], key)
+	e.Use(session.Middleware(cookieStore))
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.Logger.SetLevel(log.INFO)
 
 	www := echo.MustSubFS(assets, "www")
 
-	httpsServer.StaticFS("/", www)
-	httpsServer.FileFS("/", "index-server.html", www)
+	e.StaticFS("/", www)
+	e.FileFS("/", "index-server.html", www)
 
-	api.RegisterAuthAPI(httpsServer)
-	api.RegisterProfileAPI(httpsServer)
-	api.RegisterUsersAPI(httpsServer)
-	api.RegisterProvidersAPI(httpsServer)
-	api.RegisterBarcodesAPI(httpsServer)
-	api.RegisterProductsAPI(httpsServer)
-	api.RegisterHistoryAPI(httpsServer)
-	api.RegisterConfigAPI(httpsServer)
-	api.RegisterCheckoutAPI(httpsServer)
-
-	go func() {
-		httpServer.Logger.Fatal(httpServer.Start(":80"))
-	}()
-
-	if !utils.FileExists(certificate.CertPath) || !utils.FileExists(certificate.KeyPath) {
-		certificate.Generate()
-	}
-
-	httpsServer.Logger.Fatal(httpsServer.StartTLS(":443", certificate.CertPath, certificate.KeyPath))
+	api.RegisterAuthAPI(e)
+	api.RegisterProfileAPI(e)
+	api.RegisterUsersAPI(e)
+	api.RegisterProvidersAPI(e)
+	api.RegisterBarcodesAPI(e)
+	api.RegisterProductsAPI(e)
+	api.RegisterHistoryAPI(e)
+	api.RegisterConfigAPI(e)
+	api.RegisterCheckoutAPI(e)
 }
 
 func redirectToHTTPS() echo.MiddlewareFunc {
